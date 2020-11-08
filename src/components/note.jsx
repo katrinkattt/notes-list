1d19fdc5b23e9efeeeab6f421be014f24086ef4a
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2020-11-08 16:16:23 +0500

    Delete note.jsx

--- src/components/note.jsx
+++
@@ -1,9 +0,0 @@
-import './note.css'
-export default function Note(props) {
-    return (
-      <div className="note_main" onClick={props.func}>
-          <div className="note__title">{props.title}</div>
-          <div className="note__text">{props.text}</div>
-      </div>
-    );
-  }
