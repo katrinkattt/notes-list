d10fd1e03e3e9936a3e33620929e33149eb8db5d
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2020-11-08 16:16:50 +0500

    Delete btn.jsx

--- src/components/btn.jsx
+++
@@ -1,8 +0,0 @@
-import './btn.css'
-export default function Btn(props) {
-    return (
-        <button onClick={props.func} className="btn__main">
-            <div className="btn__text">{props.text}</div>
-        </button>
-    )
-}
